1d692603690cb49b4a93c75147ecaa6407812184
SubhashishTheCoder <subhashishpothal2004@gmail.com>
2025-07-19 17:16:38 +0530

    Implement Edit Feature

--- gui.py
+++ gui.py
@@ -5,9 +5,18 @@ import FreeSimpleGUI as sg
 label = sg.Text("Enter your To-Do here")
 input_text = sg.InputText(tooltip="Enter Todo",key="todo")
 add_button = sg.Button("Add")
+list_todos = sg.Listbox(values=functions.get_todos(),
+                        enable_events=True,
+                        size=(45,10),
+                        key="todos")
+edit_button = sg.Button("Edit")
 
 
-window = sg.Window("My To-Do App",layout=[[label],[input_text,add_button]])
+window = sg.Window("My To-Do App",
+                   layout=[[label],
+                           [input_text,add_button],
+                           [list_todos,edit_button]],
+                   font=('Helvetica',20))
 
 while True:
     event,values = window.read()
@@ -19,6 +28,21 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            list_todos.update(values=todos)
+        case "Edit" :
+            todo_to_edit = values['todos'][0]
+            todos = functions.get_todos()
+            print(todos)
+            index = todos.index(todo_to_edit)
+            print(index)
+            todos[index] = values['todo']
+            print(todos)
+            functions.write_todos(todos)
+            list_todos.update(values=todos)
+        case "todos":
+           input_text.update(value=values['todos'][0])
+
+
         case sg.WIN_CLOSED:
             break
 
